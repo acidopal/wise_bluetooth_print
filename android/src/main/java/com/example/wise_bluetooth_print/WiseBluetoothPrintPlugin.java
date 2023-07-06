0af6dbc683b7884927d7de3eba45247b766b507c
muhvarriel <muhvarriel@gmail.com>
2023-07-06 23:17:53 +0700

    change to bitmap

--- android/src/main/java/com/example/wise_bluetooth_print/WiseBluetoothPrintPlugin.java
+++ android/src/main/java/com/example/wise_bluetooth_print/WiseBluetoothPrintPlugin.java
@@ -96,12 +96,23 @@ public class WiseBluetoothPrintPlugin implements FlutterPlugin, MethodCallHandle
               socket.connect();
               outputStream = socket.getOutputStream();
               inStream = socket.getInputStream();
-
               if (!base64Image.isEmpty()) {
-                // If an image is provided, decode the Base64 string and add it to the command
+                // If an image is provided, decode the Base64 string into a Bitmap
                 byte[] decodedImageBytes = Base64.decode(base64Image, Base64.DEFAULT);
                 Bitmap bitmap = BitmapFactory.decodeByteArray(decodedImageBytes, 0, decodedImageBytes.length);
-                esc.addRastBitImage(bitmap, 576, 0);
+
+                // Scale the bitmap to the desired width and height
+                int desiredWidth = 576; // Replace with your desired width
+                int desiredHeight = 0; // Replace with your desired height or set to 0 to maintain aspect ratio
+                bitmap = Bitmap.createScaledBitmap(bitmap, desiredWidth, desiredHeight, true);
+
+                // Convert the Bitmap to a byte array for writing to the output stream
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] imageBytes = stream.toByteArray();
+
+                // Write the image bytes to the output stream
+                outputStream.write(imageBytes);
               }
 
               write(printStr);
@@ -152,5 +163,4 @@ public class WiseBluetoothPrintPlugin implements FlutterPlugin, MethodCallHandle
     }
     channel.setMethodCallHandler(null);
   }
-
 }
