59d897b3f2123ab6aae13f3fbd71a1ad11a6c9f1
muhvarriel <muhvarriel@gmail.com>
2023-07-06 23:51:27 +0700

    check new implement

--- android/src/main/java/com/example/wise_bluetooth_print/WiseBluetoothPrintPlugin.java
+++ android/src/main/java/com/example/wise_bluetooth_print/WiseBluetoothPrintPlugin.java
@@ -6,6 +6,13 @@ import android.bluetooth.BluetoothSocket;
 import android.os.Handler;
 import android.os.ParcelUuid;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import java.io.IOException;
+import android.util.Log;
+import android.content.res.AssetManager;
+import io.flutter.embedding.engine.FlutterMain;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -91,6 +98,8 @@ public class WiseBluetoothPrintPlugin implements FlutterPlugin, MethodCallHandle
               outputStream = socket.getOutputStream();
               inStream = socket.getInputStream();
 
+              printImage(result, "assets/icons/icon.png", timeout);
+
               write(printStr);
 
               // Set timeout runnable to handle timeout case
@@ -138,5 +147,217 @@ public class WiseBluetoothPrintPlugin implements FlutterPlugin, MethodCallHandle
       handler.removeCallbacks(timeoutRunnable);
     }
     channel.setMethodCallHandler(null);
+  }
+
+  private void printImageBytes(Result result, byte[] bytes) {
+    if (outputStream == null) {
+      result.error("write_error", "Not connected to a Bluetooth device", null);
+      return;
+    }
+
+    try {
+      Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+      if (bmp != null) {
+        byte[] command = Utils.decodeBitmap(bmp);
+        outputStream.write(PrinterCommands.ESC_ALIGN_CENTER);
+        outputStream.write(command);
+      } else {
+        Log.e("Print Photo error", "The file doesn't exist");
+      }
+      result.success(true);
+    } catch (IOException ex) {
+      Log.e(TAG, ex.getMessage(), ex);
+      result.error("write_error", ex.getMessage(), exceptionToString(ex));
+    }
+  }
+
+  private void printImage(Result result, String imagePath, int timeout) {
+    try {
+      byte[] imageBytes = getImageBytesFromPath(imagePath);
+      if (imageBytes != null) {
+        printImageBytes(result, imageBytes);
+      } else {
+        Log.e("Print Image error", "Failed to read image file");
+        result.success(false);
+      }
+    } catch (IOException ex) {
+      Log.e(TAG, ex.getMessage(), ex);
+      result.error("write_error", ex.getMessage(), exceptionToString(ex));
+    }
+  }
+
+  private byte[] getImageBytesFromPath(String imagePath) throws IOException {
+    AssetManager assetManager = getFlutterAssets();
+    InputStream inputStream = assetManager.open(imagePath);
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+    byte[] buffer = new byte[1024];
+    int bytesRead;
+    while ((bytesRead = inputStream.read(buffer)) != -1) {
+      bos.write(buffer, 0, bytesRead);
+    }
+
+    inputStream.close();
+    return bos.toByteArray();
+  }
+
+  private AssetManager getFlutterAssets() throws IOException {
+    Method getAssetManager = FlutterMain.class.getDeclaredMethod("getAssetManager");
+    getAssetManager.setAccessible(true);
+    return (AssetManager) getAssetManager.invoke(null);
+  }
+}
+
+public static class Utils {
+  // UNICODE 0x23 = #
+  private static final byte[] UNICODE_TEXT = new byte[] { 0x23, 0x23, 0x23,
+      0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23,
+      0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23,
+      0x23, 0x23, 0x23 };
+
+  private static String hexStr = "0123456789ABCDEF";
+  private static String[] binaryArray = { "0000", "0001", "0010", "0011",
+      "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011",
+      "1100", "1101", "1110", "1111" };
+
+  public static byte[] decodeBitmap(Bitmap bmp) {
+    int bmpWidth = bmp.getWidth();
+    int bmpHeight = bmp.getHeight();
+
+    List<String> list = new ArrayList<String>(); // binaryString list
+    StringBuffer sb;
+
+    int bitLen = bmpWidth / 8;
+    int zeroCount = bmpWidth % 8;
+
+    String zeroStr = "";
+    if (zeroCount > 0) {
+      bitLen = bmpWidth / 8 + 1;
+      for (int i = 0; i < (8 - zeroCount); i++) {
+        zeroStr = zeroStr + "0";
+      }
+    }
+
+    for (int i = 0; i < bmpHeight; i++) {
+      sb = new StringBuffer();
+      for (int j = 0; j < bmpWidth; j++) {
+        int color = bmp.getPixel(j, i);
+
+        int r = (color >> 16) & 0xff;
+        int g = (color >> 8) & 0xff;
+        int b = color & 0xff;
+
+        // if color is close to white, bit='0', else bit='1'
+        if (r > 160 && g > 160 && b > 160)
+          sb.append("0");
+        else
+          sb.append("1");
+      }
+      if (zeroCount > 0) {
+        sb.append(zeroStr);
+      }
+      list.add(sb.toString());
+    }
+
+    List<String> bmpHexList = binaryListToHexStringList(list);
+    String commandHexString = "1D763000";
+    String widthHexString = Integer.toHexString(bmpWidth % 8 == 0 ? bmpWidth / 8
+        : (bmpWidth / 8 + 1));
+    if (widthHexString.length() > 10) {
+      Log.e("decodeBitmap error", "Width is too large");
+      return null;
+    } else if (widthHexString.length() == 1) {
+      widthHexString = "0" + widthHexString;
+    }
+    widthHexString = widthHexString + "00";
+
+    String heightHexString = Integer.toHexString(bmpHeight);
+    if (heightHexString.length() > 10) {
+      Log.e("decodeBitmap error", "Height is too large");
+      return null;
+    } else if (heightHexString.length() == 1) {
+      heightHexString = "0" + heightHexString;
+    }
+    heightHexString = heightHexString + "00";
+
+    List<String> commandList = new ArrayList<String>();
+    commandList.add(commandHexString + widthHexString + heightHexString);
+    commandList.addAll(bmpHexList);
+
+    return hexList2Byte(commandList);
+  }
+
+  public static List<String> binaryListToHexStringList(List<String> list) {
+    List<String> hexList = new ArrayList<String>();
+    for (String binaryStr : list) {
+      StringBuffer sb = new StringBuffer();
+      for (int i = 0; i < binaryStr.length(); i += 8) {
+        String str = binaryStr.substring(i, i + 8);
+
+        String hexString = myBinaryStrToHexString(str);
+        sb.append(hexString);
+      }
+      hexList.add(sb.toString());
+    }
+    return hexList;
+  }
+
+  public static String myBinaryStrToHexString(String binaryStr) {
+    String hex = "";
+    String f4 = binaryStr.substring(0, 4);
+    String b4 = binaryStr.substring(4, 8);
+    for (int i = 0; i < binaryArray.length; i++) {
+      if (f4.equals(binaryArray[i]))
+        hex += hexStr.substring(i, i + 1);
+    }
+    for (int i = 0; i < binaryArray.length; i++) {
+      if (b4.equals(binaryArray[i]))
+        hex += hexStr.substring(i, i + 1);
+    }
+
+    return hex;
+  }
+
+  public static byte[] hexList2Byte(List<String> list) {
+    List<byte[]> commandList = new ArrayList<byte[]>();
+
+    for (String hexStr : list) {
+      commandList.add(hexStringToBytes(hexStr));
+    }
+    byte[] bytes = sysCopy(commandList);
+    return bytes;
+  }
+
+  public static byte[] hexStringToBytes(String hexString) {
+    if (hexString == null || hexString.equals("")) {
+      return null;
+    }
+    hexString = hexString.toUpperCase();
+    int length = hexString.length() / 2;
+    char[] hexChars = hexString.toCharArray();
+    byte[] d = new byte[length];
+    for (int i = 0; i < length; i++) {
+      int pos = i * 2;
+      d[i] = (byte) (charToByte(hexChars[pos]) << 4 | charToByte(hexChars[pos + 1]));
+    }
+    return d;
+  }
+
+  public static byte[] sysCopy(List<byte[]> srcArrays) {
+    int len = 0;
+    for (byte[] srcArray : srcArrays) {
+      len += srcArray.length;
+    }
+    byte[] destArray = new byte[len];
+    int destLen = 0;
+    for (byte[] srcArray : srcArrays) {
+      System.arraycopy(srcArray, 0, destArray, destLen, srcArray.length);
+      destLen += srcArray.length;
+    }
+    return destArray;
+  }
+
+  private static byte charToByte(char c) {
+    return (byte) "0123456789ABCDEF".indexOf(c);
   }
 }
