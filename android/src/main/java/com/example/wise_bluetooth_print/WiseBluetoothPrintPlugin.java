9b2d5c84db13ecfa2ad398b586010f4441a13891
muhvarriel <muhvarriel@gmail.com>
2023-09-11 21:08:19 +0700

    add import

--- android/src/main/java/com/example/wise_bluetooth_print/WiseBluetoothPrintPlugin.java
+++ android/src/main/java/com/example/wise_bluetooth_print/WiseBluetoothPrintPlugin.java
@@ -27,6 +27,9 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 
 public class WiseBluetoothPrintPlugin implements FlutterPlugin, MethodCallHandler {
     private MethodChannel channel;
